cf8b17518f0800e64084dbf6e0f92062015a5c8d
abhayrathor12 <rathorabhay633@gmail.com>
2026-05-18 18:02:11 +0530

    small changes

--- project/src/pages/PaymentPage.tsx
+++ project/src/pages/PaymentPage.tsx
@@ -336,8 +336,8 @@ const WebinarPage: React.FC = () => {
                                                 <rect x="3" y="4" width="18" height="18" rx="2" /><line x1="16" y1="2" x2="16" y2="6" /><line x1="8" y1="2" x2="8" y2="6" /><line x1="3" y1="10" x2="21" y2="10" />
                                             </svg>
                                         ),
-                                        label: "25th May 2026",
-                                        sub: "Monday",
+                                        label: "31st May 2026",
+                                        sub: "Sunday",
                                     },
                                     {
                                         icon: (
